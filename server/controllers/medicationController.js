e936f3ff877f0cf9a318f28df27673867be133c2
MercyKorir <mcthegreat78@gmail.com>
2023-10-24 11:01:25 +0300

    feat: medication CRUD routes and controllers

--- server/controllers/medicationController.js
+++ server/controllers/medicationController.js
@@ -1,5 +1,6 @@
 import Medication from "../models/MedicationModel.js";
 
+// Create medication
 export const createMedication = async (req, res) => {
   try {
     const { name, description, dosage } = req.body;
@@ -13,5 +14,75 @@ export const createMedication = async (req, res) => {
   } catch (err) {
     console.error("Error creating medication: ", err);
     res.status(500).json({ message: "Error creating medication" });
+  }
+};
+
+// Get all medications
+export const getAllMedications = async (req, res) => {
+  try {
+    const medications = await Medication.find({ user: req.user._id });
+    res.status(200).json({ medications });
+  } catch (err) {
+    console.error("Error getting all medications: ", err);
+    res.status(500).json({ message: "Error getting all medications" });
+  }
+};
+
+// Get medication by id
+export const getMedicationById = async (req, res) => {
+  try {
+    const medication = await Medication.findById(req.params.id);
+    res.status(200).json({ medication });
+  } catch (err) {
+    console.error("Error getting medication by id: ", err);
+    res.status(500).json({ message: "Error getting medication by id" });
+  }
+};
+
+// Update medication by id using patch
+export const updateMedicationById = async (req, res) => {
+  try {
+    const { name, description, dosage } = req.body;
+    const medication = await Medication.findById(req.params.id);
+    if (medication) {
+      medication.name = name || medication.name;
+      medication.description = description || medication.description;
+      medication.dosage = dosage || medication.dosage;
+      await medication.save();
+      res
+        .status(200)
+        .json({ message: "Medication updated successfully", medication });
+    } else {
+      res.status(404).json({ message: "Medication not found" });
+    }
+  } catch (err) {
+    console.error("Error updating medication by id: ", err);
+    res.status(500).json({ message: "Error updating medication by id" });
+  }
+};
+
+// Delete medication by id
+export const deleteMedicationById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = req.user._id;
+    const medication = await Medication.findById(id);
+    if (medication) {
+      if (medication.user.toString() !== user.toString()) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const deletedMedication = await Medication.findByIdAndRemove(id);
+      if (!deletedMedication) {
+        return res
+          .status(404)
+          .json({ message: "Error deleting medication by id" });
+      }
+      res.status(200).json({ message: "Medication deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Medication not found" });
+    }
+  } catch (err) {
+    console.error("Error deleting medication by id: ", err);
+    res.status(500).json({ message: "Error deleting medication by id" });
   }
 };
--- server/routes/medication.js
+++ server/routes/medication.js
@@ -1,9 +1,28 @@
 import express from "express";
-import { createMedication } from "../controllers/medicationController.js";
+import {
+  createMedication,
+  getAllMedications,
+  getMedicationById,
+  updateMedicationById,
+  deleteMedicationById,
+} from "../controllers/medicationController.js";
 import { authenticateJWT } from "../middleware/authentication.js";
 
 const router = express.Router();
 
+// Create medication
 router.post("/create", authenticateJWT, createMedication);
+
+// Get all medications
+router.get("/all", authenticateJWT, getAllMedications);
+
+// Get medication by id
+router.get("/:id", authenticateJWT, getMedicationById);
+
+// Update medication by id using patch
+router.patch("/:id", authenticateJWT, updateMedicationById);
+
+// Delete medication by id
+router.delete("/:id", authenticateJWT, deleteMedicationById);
 
 export default router;
